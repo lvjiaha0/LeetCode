b378b26427c4838285512cafc3f9adb7a373e1ee
Louis Lv <louis.lv@grapecity.com>
2019-01-25 20:20:32 +0900

    Update AddTwoNumbers

--- Problems/AddTwoNumbers/AddTwoNumbers/Program.cs
+++ Problems/AddTwoNumbers/AddTwoNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 
 //给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
@@ -32,7 +33,52 @@ namespace AddTwoNumbers
 
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
+            ListNode headNode1 = l1;
+            ListNode headNode2 = l2;
+        }
 
+        public ListNode Revert(ListNode node, ListNode currentNode)
+        {
+            ListNode headNode = node;
+            ListNode preNode = headNode.next;
+            ListNode newNode = preNode.next;
+            while (newNode != null && Put(headNode, preNode, newNode))
+            {
+                preNode
+            }
+
+        }
+
+        public bool Put(ListNode headNode, ListNode preNode, ListNode newNode)
+        {
+            bool res = false;
+            ListNode deletedNode = DeleteNext(preNode);
+            if (deletedNode != null)
+            {
+                InsertAfterHead(headNode, deletedNode);
+                res = true;
+            }
+            return res;
+        }
+
+
+        public void InsertAfterHead(ListNode headNode, ListNode newNode)
+        {
+            ListNode nextNode = headNode.next;
+            headNode.next = newNode;
+            newNode.next = nextNode;
+        }
+
+        public ListNode DeleteNext(ListNode preNode)
+        {
+            ListNode deletedNode = preNode.next;
+            if (deletedNode != null)
+            {
+                ListNode nextNode = deletedNode.next;
+                preNode.next = nextNode;
+                deletedNode.next = null;
+            }
+            return deletedNode;
         }
     }
 
