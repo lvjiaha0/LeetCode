7b1570e3b03508e4ec2b9af2d090c3e30ac0292a
lvjiaha0 <jiaha0@163.com>
2021-03-02 18:59:09 +0900

    【TwoSum】 两数之和 添加注释

--- Problems/TwoSum/TwoSum/Program.cs
+++ Problems/TwoSum/TwoSum/Program.cs
@@ -3,6 +3,23 @@ using System.Collections.Generic;
 
 //给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
 //你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+//示例 1：
+
+//输入：nums = [2, 7, 11, 15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回[0, 1] 。
+//示例 2：
+
+//输入：nums = [3, 2, 4], target = 6
+//输出：[1,2]
+//示例 3：
+
+//输入：nums = [3, 3], target = 6
+//输出：[0,1]
+
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/two-sum
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 namespace TwoSum
 {
     class Program
